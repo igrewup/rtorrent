f41168b8d1a0d9bbab3ecca710abb78204b835aa
igrewup <igrewup@users.noreply.github.com>
2014-12-31 03:29:54 -0500

    Update rtorrent-beta.sh

--- rtorrent-beta.sh
+++ rtorrent-beta.sh
@@ -1,5 +1,1 @@
-# Installing dependencies
-apt-get update
-apt-get -y install openssl git subversion apache2 apache2-utils build-essential libsigc++-2.0-dev libcurl4-openssl-dev automake libtool libcppunit-dev libncurses5-dev libapache2-mod-scgi php5 php5-curl php5-cli libapache2-mod-php5 screen rar unrar zip unzip
-
 
