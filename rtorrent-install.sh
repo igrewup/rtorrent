9e64d1758b3ba34af17b71f969657a31ac73c572
igrewup <igrewup@users.noreply.github.com>
2015-01-03 15:28:58 -0500

    Update rtorrent-install.sh

--- rtorrent-install.sh
+++ rtorrent-install.sh
@@ -98,7 +98,7 @@ echo
 echo "USERNAME: $user |  HOMEDIR: $homedir"
 echo
 
-if [ "$INSTALLRTORRENT1" = "YES" ]; then echo " Install/Update RTORRENT: $INSTALLRTORRENT1"; echo fi
+if [ "$INSTALLRTORRENT1" = "YES" ]; then echo " Install/Update RTORRENT: $INSTALLRTORRENT1"; echo; fi
 if [ "$INSTALLRUTORRENT1" = "YES" ] && [ "$INSTALLPLUGINS1" = "YES" ]; then
 	echo " Install/Update RUTORRENT: $INSTALLRUTORRENT1 and PLUGINS: $INSTALLPLUGINS1"
 	echo
