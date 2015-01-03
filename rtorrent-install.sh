d8c4658c35d85e195036632dac97efb4693aa08e
igrewup <igrewup@users.noreply.github.com>
2015-01-03 15:26:58 -0500

    Update rtorrent-install.sh

--- rtorrent-install.sh
+++ rtorrent-install.sh
@@ -99,7 +99,13 @@ echo "USERNAME: $user |  HOMEDIR: $homedir"
 echo
 
 if [ "$INSTALLRTORRENT1" = "YES" ]; then echo " Install/Update RTORRENT: $INSTALLRTORRENT1";echo fi
-if [ "$INSTALLRUTORRENT1" = "YES" ]; then echo " Install/Update RUTORRENT: $INSTALLRUTORRENT1 and PLUGINS: $INSTALLPLUGINS1"; echo; else echo "Install/Update RUTORRENT: $INSTALLRUTORRENT1"; echo; fi
+if [ "$INSTALLRUTORRENT1" = "YES" ] && [ "$INSTALLPLUGINS1" = "YES" ]; then
+	echo " Install/Update RUTORRENT: $INSTALLRUTORRENT1 and PLUGINS: $INSTALLPLUGINS1"
+	echo
+else
+	echo "Install/Update RUTORRENT: $INSTALLRUTORRENT1"
+	echo
+fi
 if [ "$INSTALLSSH1" = "YES" ]; then echo " Install/Update SSH: $INSTALLSSH1 on PORT: $NEWSSHPORT1"; echo; fi
 if [ "$INSTALLVSFTPD1" = "YES" ]; then echo " Install/Update VSFTPD: $INSTALLVSFTPD1 on PORT: $NEWFTPPORT1"; echo; fi
 if [ "$INSTALLSQUID1" = "YES" ]; then echo " Install/Update Proxy Server: $INSTALLSQUID1 on PORT: $SQUIDPORT1"; echo; fi
