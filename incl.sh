1c0c1e20fae8882a7ae3afb92f13b3c4faeb38d3
igrewup <igrewup@users.noreply.github.com>
2015-01-23 02:09:36 -0500

    Update incl.sh

--- incl.sh
+++ incl.sh
@@ -111,6 +111,28 @@ YESNO=$YESYES$NONO
   eval $RETURN=\$NEWVAR1
 }
 
+# Check webserver root to make sure it's secure.
+if [ ! -f /var/www/index.html ]; then
+echo "Webserver root folder is not secure, securing /var/www/"
+echo '<html><body><h1>It works!</h1>
+<p>This is the default web page for this server.</p>
+<p>The web server software is running but no content has been added, yet.</p>
+</body></html>' > /var/www/index.html
+chown root.root /var/www/index.html
+chmod 444 /var/www/vpn/index.html
+echo "Webserver root folder /var/www/ is now secure."
+$HITENTER
+fi
+
+if [ ! -f /var/www/.htaccess ]; then
+echo "Redirect is missing."
+echo 'Redirect 301 /index.html http://www.torproject.org' > /var/www/.htaccess
+chown root.root /var/www/.htaccess
+chmod 444 /var/www/.htaccess
+echo "Redirect now setup."
+$HITENTER
+fi
+
 # Check to make sure VPN folder is secure.
 if [ ! -f /var/www/vpn/.htaccess ]; then
 mkdir -p /var/www/vpn/
