c35d081a65f6a3cd3e5cbd3d9ab7b44e8f1605c6
igrewup <igrewup@users.noreply.github.com>
2015-02-04 08:27:23 -0500

    Update block-sites.sh

--- block-sites.sh
+++ block-sites.sh
@@ -2,12 +2,27 @@
 # Install whois
 sudo apt-get -y install whois
 
-# Block facebook using IPTABLES
 blocked="/root/blocked-sites-list.txt"
+rm $blocked && touch $blocked
+
+# Allow SSH
+echo "# Start Allow SSH" >> $blocked
+echo "iptables -A INPUT -i eth0 -p tcp --dport 22 -j ACCEPT" >> $blocked
+echo "# End Allow SSH" >> $blocked
+
+# Block Facebook
+echo "# Start Blocking Facebook" >> $blocked
 for ip in `whois -h whois.radb.net '!gAS32934' | grep /`
 do
-rm $blocked && touch $blocked
-echo $ip >> $blocked
-cat $blocked
+echo "iptables -A FORWARD -p all -d $ip -j REJECT" >> $blocked
 #  /sbin/iptables -A FORWARD -p all -d $ip -j REJECT
 done
+echo "# End Blocking Facebook" >> $blocked
+
+# Block POF
+echo "# Start Blocking POF" >> $blocked
+echo "iptables -A FORWARD -p all -d 199.182.216.166 -j REJECT" >> $blocked
+echo "# End Blocking POF" >> $blocked
+
+clear
+cat $blocked
