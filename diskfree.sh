a6040f459098b35c13d735587a33756eb06287c7
igrewup <igrewup@users.noreply.github.com>
2015-05-13 00:03:56 -0400

    Update diskfree.sh

--- diskfree.sh
+++ diskfree.sh
@@ -2,9 +2,9 @@
 # Run every 10 minutes, add the command below to crontab.
 # Use command: crontab -e
 # Add the line below to crontab (without the # at the begining)
-# */10 * * * * /path/to/script
+# */10 * * * * /path/to/diskfree.sh
 # change the location where df will check for free space.
-diskcheck="/change/me"
+diskcheck="/location/of/dir/to/check/freespace"
 # Do not modify anything starting here.
 df -h $diskcheck | tail -n+2 | while read fs size used avail rest ; do
     if [[ $used ]] ; then
