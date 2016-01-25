aaa84d5efe6be269d259004b595fc291b69b5a68
igrewup <igrewup@users.noreply.github.com>
2016-01-25 01:44:36 -0500

    Update check_version.sh

--- check_version.sh
+++ check_version.sh
@@ -3,7 +3,9 @@ source $(dirname $0)/incl.sh
 
 dir=$(dirname $0)
 
+if [[ -f $ /tmp/LASTESTVERSION ]]; then
 rm /tmp/LATESTVERSION
+fi
 
 if [[ ! -f $dir/VERSION ]]; then
 	echo "Unable to locate local VERSION file."
