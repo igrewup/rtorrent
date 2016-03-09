925db55210f37dd35e32f55170a36141e095297a
igrewup <igrewup@users.noreply.github.com>
2016-03-09 17:52:44 -0500

    Update incl.sh

--- incl.sh
+++ incl.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 OS1=$(lsb_release -si)
 OS2=$(lsb_release -sc)
 OSV1=$(lsb_release -rs)
@@ -110,34 +109,3 @@ YESNO=$YESYES$NONO
   done
   eval $RETURN=\$NEWVAR1
 }
-
-# Check webserver root to make sure it's secure.
-if [ ! -f /var/www/index.html ]; then
-echo "Webserver root folder is not secure, securing /var/www/"
-echo '<html><body><h1>It works!</h1>
-<p>This is the default web page for this server.</p>
-<p>The web server software is running but no content has been added, yet.</p>
-</body></html>' > /var/www/index.html
-chown root.root /var/www/index.html
-chmod 444 /var/www/index.html
-echo "Webserver root folder /var/www/ is now secure."
-fi
-
-if [ ! -f /var/www/.htaccess ]; then
-echo "Redirect is missing."
-echo 'Redirect 301 /index.html http://www.torproject.org' > /var/www/.htaccess
-chown root.root /var/www/.htaccess
-chmod 444 /var/www/.htaccess
-echo "Redirect now setup."
-fi
-
-# Check to make sure VPN folder is secure.
-if [ ! -f /var/www/vpn/.htaccess ]; then
-mkdir -p /var/www/vpn/
-echo "VPN folder is not secure, securing /var/www/vpn."
-echo '#Prevent directory listings
-Options All -Indexes' > /var/www/vpn/.htaccess
-chown www-data.www-data /var/www/vpn/.htaccess
-chmod 444 /var/www/vpn/.htaccess
-echo "VPN folder /var/www/vpn is now secure."
-fi
